3456e7d5550f283843ef3b6bb5851fd44cb72a64
natadekoko68 <nikotaro3100@gmail.com>
2023-12-18 00:07:55 +0900

    Secondary commit

--- day3.py
+++ day3.py
@@ -13,11 +13,11 @@ standard_abs = {"PE": [0.000, 0.073, 0.161, 0.356],
                 "PS": [0.000, 0.007, 0.064, 0.093, 0.211],
                 }  # a.u.
 
-mokuteki_abs = {"PE": 0.095,
+objective_abs = {"PE": 0.095,
                 "PS": 0.007,
                 }  # a.u.
 
-mokuteki_quantity = {"PE": 4.7,
+objective_quantities = {"PE": 4.7,
                      "PS": 3.9,
                      }  # ml
 
@@ -36,16 +36,25 @@ def graph(key):
     popt, _ = op.curve_fit(f, standard_concs[key], standard_abs[key])
     x = make_linspace(key)
     y_pred = popt[0] * x + popt[1]
+    x_obj = (objective_abs[key] - popt[1])/popt[0]
     r2 = r2_score(standard_abs[key], np.array(standard_concs[key]) * popt[0] + popt[1])
-    plt.scatter(standard_concs[key], standard_abs[key], marker=".", label=key, color='black')
-    plt.plot(x, y_pred, label=f'Fit: R$^{{{2}}}$={r2:.3f}', color='red')
+    fig = plt.figure(tight_layout=True, figsize=(8, 6), dpi=300)
+    ax = fig.add_subplot(1, 1, 1)
+    plt.scatter(standard_concs[key], standard_abs[key], marker=".", label=f"{key}検量用サンプル", color='black')
+    plt.plot(x, y_pred, label=f'Fit: R$^{{{2}}}$={r2:.3f}', color='blue')
+    plt.scatter(x_obj, objective_abs[key], color='red', label=f"濃度:{x_obj:.3f}, 吸光度:{objective_abs[key]:.3f}")
     text = f"y = {popt[0]:.4f}x + {popt[1]:.4f}" if popt[1] > 0 else f"y = {popt[0]:.4f}x - {abs(popt[1]):.4f}"
     plt.text(max(standard_concs[key]) / 2, (popt[0] * max(standard_concs[key]) / 2 + popt[1]) * 0.8, text)
     plt.title(f"検量線 ({key})")
     plt.legend()
     plt.xlabel("濃度 (mM)")
     plt.ylabel("吸光度 (a.u.)")
-    plt.tight_layout()
+    plt.xlim(ax.get_xlim())
+    plt.ylim(ax.get_ylim())
+    plt.plot([x_obj, x_obj], [ax.get_ylim()[0], objective_abs[key]], ":")
+    plt.plot([ax.get_xlim()[0], x_obj], [objective_abs[key], objective_abs[key]], ":")
+    plt.text(x_obj, ax.get_ylim()[0],f"{x_obj:.3f}", horizontalalignment='left', verticalalignment='bottom')
+    plt.text(ax.get_xlim()[0], objective_abs[key] ,f"{objective_abs[key]:.3f}", horizontalalignment='left', verticalalignment='bottom')
     plt.savefig(f"/Users/kotaro/Desktop/Day3({key}).jpg", dpi=300)
     plt.show()
 
