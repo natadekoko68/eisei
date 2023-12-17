ea4ac002e659e0c7bf68823b404e8e50c60e033d
natadekoko68 <nikotaro3100@gmail.com>
2023-12-17 21:51:39 +0900

    Secondary commit

--- day3.py
+++ day3.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import japanize_matplotlib
 import scipy.optimize as op
+from sklearn.metrics import r2_score
 
 samples = ["PE","PS"]
 
@@ -23,13 +24,11 @@ mokuteki_quantity = {"PE" : 4.7,
 def f(x,a,b):
     return a * x + b
 
+
 def make_linspace(key,num=100):
     max_temp = max(kenryosen_concs[key])
     min_temp = min(kenryosen_concs[key])
     return np.linspace(min_temp,max_temp,num)
-
-
-from sklearn.metrics import r2_score
 
 
 def graph(key):
@@ -37,18 +36,23 @@ def graph(key):
     x = make_linspace(key)
     y_pred = popt[0] * x + popt[1]
     r2 = r2_score(kenryosen_abs[key], np.array(kenryosen_concs[key])*popt[0] + popt[1])
-    plt.scatter(kenryosen_concs[key], kenryosen_abs[key], marker=".", label=key)
-    plt.plot(x, y_pred, label=f'Fit: R2={r2:.4f}', color='black')
-    plt.title(f"検量線({key})")
+    plt.scatter(kenryosen_concs[key], kenryosen_abs[key], marker=".", label=key, color='black')
+    plt.plot(x, y_pred, label=f'Fit: R$^{{{2}}}$={r2:.3f}', color='red')
+    text = f"y = {popt[0]:.4f}x + {popt[1]:.4f}" if popt[1] > 0 else f"y = {popt[0]:.4f}x - {abs(popt[1]):.4f}"
+    plt.text(max(kenryosen_concs[key])/2,(popt[0] * max(kenryosen_concs[key])/2 + popt[1])*0.8,text)
+    plt.title(f"検量線 ({key})")
     plt.legend()
     plt.xlabel("濃度 (mM)")
     plt.ylabel("吸光度 (a.u.)")
     plt.tight_layout()
+    plt.savefig(f"/Users/kotaro/Desktop/Day3({key}).jpg", dpi=300)
     plt.show()
+
 
 def main():
     for key in samples:
         graph(key)
+
 
 if __name__=="__main__":
     main()
---
+++ day5.py
@@ -0,0 +1,40 @@
+import matplotlib.pyplot as plt
+import japanize_matplotlib
+
+fluorescent = {"No.1": 1.801,
+               "No.2": 1.817,
+               "No.3": 1.729,
+               "No.4": 1.926,
+               "No.5": 1.840,
+               "No.6": 2.107,
+               "No.7": 1.390,
+               "No.8": 1.561,
+               "No.9": 1.375,
+               "No.10": 1.880,
+               "No.11": 1.449,
+               "No.12": 1.230,
+               "No.13": 2.013,
+               "No.14": 0.814,
+               }
+
+
+def to_percentage(key, background="No.14", maximum="No.13"):
+    return 100 * (fluorescent[key] - fluorescent[background]) / (fluorescent[maximum] - fluorescent[background])
+
+
+def main():
+    assert (len(fluorescent) == 14)
+    labels = [f"No.{i}" for i in range(1, 15)]
+    percentage = list(map(to_percentage, fluorescent))
+    fig = plt.figure(tight_layout=True, figsize=(12, 6), dpi=300)
+    plt.bar([i for i in range(1, 15)], percentage, tick_label=labels)
+    plt.title("各サンプルにおけるヒスタミン放出率", size=15)
+    plt.ylabel(r"割合 ($\%$)")
+    plt.xlabel("サンプル")
+    plt.grid(axis="y", linestyle="dotted", color="gray")
+    plt.savefig("/Users/kotaro/Desktop/Day5.jpg", dpi=300)
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()
