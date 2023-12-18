84c3e19f44c7723fffb250662112f1544289e346
natadekoko68 <nikotaro3100@gmail.com>
2023-12-18 18:14:27 +0900

    Changes

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from packages.chromatgraphy import get_concat_pictures
 
 if __name__ == "__main__":
     outputpath = "/Users/kotaro/Desktop/"
+    # outputpath = ""
     day3(outputpath=outputpath)
     day5(outputpath=outputpath)
     get_concat_pictures(outputpath=outputpath)
